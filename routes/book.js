1b50ed44a78354ccbac13f02700a1e1c3c7a0017
katalinapalma <kata_92@hotmail.com>
2020-05-19 08:40:31 +0200

    getBookById and post methods done

--- routes/book.js
+++ routes/book.js
@@ -2,8 +2,37 @@ get = (req, res, next) => {
   req.models.Book.find().then((books) => {
       return res.send(books);
     }).catch((error) => next(error))
-}
+};
+
+getBookById = (req, res, next) => {
+  req.models.Book.findById(req.params.id).then((book) => {
+    return res.send(book)
+  }).catch((error) => {
+    next(error)
+  })
+};
+
+postBook = (req, res, next) => {
+  req.models.Book.create({
+    isbn: req.body.isbn,
+    title: req.body.title,
+    author: req.body.author,
+    price: req.body.price,
+    selleremail: req.body.selleremail,
+    used: req.body.used,
+    location: {
+      city: req.body.city,
+      street: req.body.street,
+    }
+  }).then((book) => {
+    return res.status(201).send(book)
+  }).catch((error) => {
+    next(error)
+  })
+};
 
 module.exports = {
   get,
+  getBookById,
+  postBook,
 }
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 
-const book = require('./book.js')
+const book = require('./book.js');
 
 router.get("/books", book.get)
+router.get("/books/:id", book.getBookById);
+router.post("/books", book.postBook);
 
 module.exports = router
