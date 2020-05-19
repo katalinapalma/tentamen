f19cac04d0a73bc3fb4826ff464380c53aca0eee
katalinapalma <kata_92@hotmail.com>
2020-05-19 08:25:46 +0200

    mongoose schema done

--- models/book.js
+++ models/book.js
@@ -1,6 +1,16 @@
 mongoose = require('mongoose');
 
 const bookSchema = new mongoose.Schema({
+    isbn: Number,
+    title: String,
+    author: String,
+    price: Number,
+    selleremail: String,
+    used: Boolean,
+    location: {
+        city: String,
+        street: String,
+    } 
 });
 
 const Book = mongoose.model('Book', bookSchema);
